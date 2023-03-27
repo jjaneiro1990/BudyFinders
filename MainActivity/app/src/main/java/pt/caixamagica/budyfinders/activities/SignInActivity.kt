49cff211a984a819a4a0cc37235050f90445e8b5
joaojaneiro <joao.janeiro@caixamagica.pt>
2023-03-27 18:07:50 +0100

    Add sign in and sign up

---
+++ MainActivity/app/src/main/java/pt/caixamagica/budyfinders/activities/SignInActivity.kt
@@ -0,0 +1,12 @@
+package pt.caixamagica.budyfinders.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pt.caixamagica.budyfinders.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
